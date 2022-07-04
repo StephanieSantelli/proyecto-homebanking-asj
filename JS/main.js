1ca15061f6a70f8fddcfda3be9b765463897ddd4
Stephanie Santelli <ssantelli@petersen.corp>
2022-07-04 12:33:41 -0300

    primer commit del homebanking

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+function recuperar(){
+    document.getElementById('recupera-contra').innerText= '<p>Se ha enviado un correo electrónico de recuperación de contraseña. Verifique su casilla de mail</p>'
+
+}
