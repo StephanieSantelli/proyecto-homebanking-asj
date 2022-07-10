93a0a6d86321fa1306e8c72d967759e11ac89e08
Stephanie Santelli <ssantelli@petersen.corp>
2022-07-10 04:48:14 -0300

    slider, fondos, tablas, todo hecho a nuevo

--- JS/main.js
+++ JS/main.js
@@ -8,23 +8,120 @@ let button = document.getElementById("cajita")
 let cajita = 0;
 
 
-function cajainvisible(id){
-   const caja = document.getElementById(id)
-    if (caja.style.visibility === "hidden"){
-        caja.style.visibility = "visible"
-    } else {
-        caja.style.visibility = "hidden"
 
+function cajainvisible(id){
+    const tablas = document.getElementsByClassName("secciontabla")
+    for (let i = 0; i < tablas.length; i++){
+        console.log(tablas[i]);
+        tablas[i].style.display = "none"
+    }
+
+   const caja = document.getElementById(id)
+   console.log(caja.style.display);
+    if (caja.style.display === "none" || !caja.style.display){
+        caja.style.display = "flex";
+    } else {
+        caja.style.display = "none";
     }
 }
 
+/* onclick="reemplazo()" */
+//jQuery
+
+$('#usuarioIngreso').click(reemplazo)
 function reemplazo(){
-    let registro = document.getElementById("usuarioIngreso");
     let nombreIngresado = prompt('Ingresá tu nombre');
-    registro.innerHTML = nombreIngresado? "Bienvenido " + nombreIngresado: "Bienvenido";
-     
+    $(this).text(nombreIngresado? "Bienvenido " + nombreIngresado: "Bienvenido");
 }
    
+/* slider*/
+
+let indiceDiapo = 1;
+
+mostrarDiapos(indiceDiapo) /* llamo la funcion que muestra la diapo en el indiceDiapo*/
+
+function mostrarDiapos(n){ /* defino la funcion que muestra la diapo. el 'n' es indice de la imagen*/
+ let diapositivas = document.getElementsByClassName("diapos"); /*cargo las diapo desde el html*/
+ let puntos = document.getElementsByClassName("punto"); /*cargo los puntitos del htmb*/
+ if (n>diapositivas.length){
+    indiceDiapo = 1;
+ }else if (n<1){
+    indiceDiapo= diapositivas.length; //este if, se ocupa de que los indices estén correctos
+ }else{
+    indiceDiapo = n;
+ }
+
+ for (let i = 0; i < diapositivas.length; i++){ //este for pone a todas las diapos ocultas
+    diapositivas[i].style.display = "none";
+ }
+    diapositivas[indiceDiapo-1].style.display = "block"; //este menos uno hace que se vea la imagen desde la posicion 0
+
+for (let i = 0; i< puntos.length; i++){ // hago lo mismo pero ahora con los puntitos
+    puntos[i].className= puntos[i].className.replace(" activo", "") //aca le saco la clase activo a todos los puntos
+    
+}
+    puntos[indiceDiapo-1].className+= " activo"; // defino que el punto activo sea el que se encuentra en la posicion de la diapo mostrada
+    
+
+    //setTimeout( function(){
+      //  mostrarDiapos(indiceDiapo+1)
+    //} , 6000) // esto es para que cada 6 segundos se llame a la funcion con el siguiente valor del indice
+
+
+}  
+    setInterval(function(){
+        moverDiapo(1)
+    }, 6000)
+
+function moverDiapo(n){ //defino la funcion que servira para que la diapo se mueva sola
+    indiceDiapo = indiceDiapo+n; //le paso como parámeto un número, si suma un "n" avanza, si resta un "n" retrocede, n=1; o -1
+    mostrarDiapos(indiceDiapo); //pido q me muestre la imagen 
+}
+
+function irAdiapo(n) {
+    indiceDiapo = n; //cambia el indice de la imagen
+    mostrarDiapos(indiceDiapo); //muestra la imagen en el indice nuevo ya recien guardado
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    /*
      if (cajita == 0){
         caja.style.visibility = "hidden"
